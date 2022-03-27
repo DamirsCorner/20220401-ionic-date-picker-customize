f4c38774f91aa670cd2695fbb4ceb85a771f492f
Damir Arh <damir.arh@gmail.com>
2022-03-27 18:36:58 +0200

    Custom buttons are affected by global styles

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonDatetime } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonDatetime, { static: true }) datePicker: IonDatetime;
+
   date: Date;
 
   constructor() {}
+
+  cancel() {
+    this.datePicker.cancel();
+  }
+
+  done() {
+    this.datePicker.confirm();
+  }
 }
